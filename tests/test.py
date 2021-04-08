a8a43d37c14247c72ca68635e9dd8cd67b9b9918
xadhrit <maharajrabel@gmail.com>
2021-04-08 11:16:21 -0400

    some progress and i am fucking tired but i will do it no matter what

---
+++ tests/test.py
@@ -0,0 +1,32 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from google_trans_new import google_translator
+
+pdfFileObj = open("tor-design.pdf", "rb")
+pdfReader = PdfFileReader(pdfFileObj)
+
+lang = input('Enter target language : ')
+
+# no of pages
+num_pages = pdfReader.numPages
+print( "No of pages" , num_pages)
+t = google_translator(timeout=10)
+page_content=[]
+#extracting text from each  page
+for p in range(num_pages):
+    page = pdfReader.getPage(p)
+    text = page.extractText()
+    #print(text)
+    translation = t.translate(text.split(" "), lang)
+    print(len(translation))
+    """for line in range(len(translation)):
+        result_text = translation[line].replace("\n\n", " ")
+        page_content.append(result_text)
+        print(page_content)
+    """
+   
+
+
+
+
+
+
