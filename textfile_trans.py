e8e9b9a5c02f10ad890d631c4a1cb8cc1942dd37
xadhrit <maharajrabel@gmail.com>
2021-04-09 03:53:50 -0400

    text file translator

--- textfile_trans.py
+++ textfile_trans.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python
+import colorama
 
+#colors
+SB = '\033[96m'  #skyblue
+P = '\033[95m'  #pink
+B = '\033[94m'  #blue
+Y = '\033[93m'  #yellow
+G= '\033[92m'   #green
+R = '\033[91m'  #red
+W = '\033[90m' #white
+
+
+print("""%s
+        tttt
+        t  t
+        t  t       %s
+   tttttt  tttttt     rrrrrrrrr
+  tttttt %s tttttt   rrrrrrrr
+        t  t        rrr
+        t  t        rrr
+        t  t        rrr                        ANS
+        tttt        rrr
+
+                                          (TRANSLATE PDFS AND  TEXT FILES)
+                                          %sby Adhrit
+                                          [twitter : https://twitter.com/xadhrit]
+                                          [github : https://github.com/xadhrit]
+
+       """%(R,Y,SB,G))
 
 
 import os
-
 from google_trans_new import google_translator
 from multiprocessing.dummy import Pool as ThreadPool
 import time
-import colorama
+
 
 pool = ThreadPool(8) 
 
-lang=input("Enter the target language :  ")
-                                                 
+lang=input( R + "Enter the target language :  ")
+
+_path = input(str( Y  + "Enter target file's path : "))
+
 
 def request(text):
     t = google_translator(timeout=5)
@@ -22,8 +51,8 @@ def request(text):
 def main():
     time1 = time.time()
     print( G  +  "This method is good enough for big text files")
-
-    with open('../source/input.txt', 'r') as fp:
+    name, ext = os.path.splitext(_path)     
+    with open(_path, 'r') as fp:
         texts = fp.readlines()
         #texts = list(texts)
         try:
@@ -35,13 +64,14 @@ def main():
         pool.join()
 
         time2 = time.time()
-        print("Translating %s sentenctes, a total of %ss"%(len(texts), time2-time1))
+        print( SB +  "Translating %s sentenctes, a total of %ss"%(len(texts), time2-time1))
         fp.close()
-
-        ft = open('../source/result.txt', "w")
+        result_file = f'{name}_{lang}.txt'
+        ft = open(result_file, "w")
         L = texts
         ft.write(str(results))
         #ft.writelines(L)
         ft.close()
+        print(G+'Done, success!')
 if __name__ == "__main__":
     main()
